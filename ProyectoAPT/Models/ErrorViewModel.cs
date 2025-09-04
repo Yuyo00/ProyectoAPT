fceb41a57b4df67a267520b62825dbf6b4c2d725
Antonia Mardones <ant.mardones@duocuc.cl>
2025-09-03 20:34:33 -0400

    adding files

---
+++ ProyectoAPT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoAPT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
