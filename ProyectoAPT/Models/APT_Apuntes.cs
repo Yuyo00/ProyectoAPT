d2131c6bf5145c038ef7fef59ea15a0e2a6b4180
Antonia Mardones <ant.mardones@duocuc.cl>
2025-09-25 19:28:49 -0300

    Cmabios

---
+++ ProyectoAPT/Models/APT_Apuntes.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoAPT.Models
+{
+    public class APT_Apuntes
+    {
+        public int ID_Apunte { get; set; }
+        public int COD_Ramo { get; set; }
+        public string DESC_Texto { get; set; }
+        public string DESC_Titulo { get; set; }
+        public DateTime DTTM_FechaClase { get; set; }
+    }
+}
---
+++ ProyectoAPT/Models/APT_Eventos.cs
@@ -0,0 +1,13 @@
+﻿namespace ProyectoAPT.Models
+{
+    public class APT_Eventos
+    {
+        public int ID_Evento { get; set; }
+        public string COD_Usuario { get; set; }
+        public DateTime DTTM_FechaEvento { get; set; }
+        public bool FLG_Notificar { get; set; }
+        public int COD_TipoNotificacion { get; set; }
+        public string DESC_Nombre { get; set; }
+        public int MNT_Porcentaje { get; set; }
+    }
+}
